209b42a7c0e192126cdf83042cc8734258af4f46
Hai Yan <oeyh@amazon.com>
2024-04-27 16:54:25 -0500

    Add Rds source and service
    Signed-off-by: Hai Yan <oeyh@amazon.com>

--- data-prepper-plugins/rds-source/src/main/java/org/opensearch/dataprepper/plugins/source/rds/RdsSource.java
+++ data-prepper-plugins/rds-source/src/main/java/org/opensearch/dataprepper/plugins/source/rds/RdsSource.java
@@ -5,5 +5,66 @@
 
 package org.opensearch.dataprepper.plugins.source.rds;
 
-public class RdsSource {
+import org.opensearch.dataprepper.metrics.PluginMetrics;
+import org.opensearch.dataprepper.model.annotations.DataPrepperPluginConstructor;
+import org.opensearch.dataprepper.model.buffer.Buffer;
+import org.opensearch.dataprepper.model.event.Event;
+import org.opensearch.dataprepper.model.record.Record;
+import org.opensearch.dataprepper.model.source.Source;
+import org.opensearch.dataprepper.model.source.coordinator.SourcePartitionStoreItem;
+import org.opensearch.dataprepper.model.source.coordinator.enhanced.EnhancedSourceCoordinator;
+import org.opensearch.dataprepper.model.source.coordinator.enhanced.EnhancedSourcePartition;
+import org.opensearch.dataprepper.model.source.coordinator.enhanced.UsesEnhancedSourceCoordination;
+import org.opensearch.dataprepper.plugins.source.rds.coordination.PartitionFactory;
+import org.opensearch.dataprepper.plugins.source.rds.coordination.partition.LeaderPartition;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Objects;
+import java.util.function.Function;
+
+public class RdsSource implements Source<Record<Event>>, UsesEnhancedSourceCoordination {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RdsSource.class);
+
+    private final PluginMetrics pluginMetrics;
+    private final RdsSourceConfig sourceConfig;
+    private EnhancedSourceCoordinator sourceCoordinator;
+    private RdsService rdsService;
+
+    @DataPrepperPluginConstructor
+    public RdsSource(final PluginMetrics pluginMetrics, final RdsSourceConfig sourceConfig) {
+        this.pluginMetrics = pluginMetrics;
+        this.sourceConfig = sourceConfig;
+    }
+
+    @Override
+    public void start(Buffer<Record<Event>> buffer) {
+        Objects.requireNonNull(sourceCoordinator);
+        sourceCoordinator.createPartition(new LeaderPartition());
+
+        rdsService = new RdsService(sourceCoordinator, sourceConfig, pluginMetrics);
+
+        LOG.info("Start RDS service");
+        rdsService.start(buffer);
+    }
+
+    @Override
+    public void stop() {
+        LOG.info("Stop RDS service");
+        if (Objects.nonNull(rdsService)) {
+            rdsService.shutdown();
+        }
+    }
+
+    @Override
+    public void setEnhancedSourceCoordinator(EnhancedSourceCoordinator sourceCoordinator) {
+        this.sourceCoordinator = sourceCoordinator;
+        this.sourceCoordinator.initialize();
+    }
+
+    @Override
+    public Function<SourcePartitionStoreItem, EnhancedSourcePartition> getPartitionFactory() {
+        return new PartitionFactory();
+    }
 }
